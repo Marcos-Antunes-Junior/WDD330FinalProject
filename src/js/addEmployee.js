a46e720331d6fc6ae8d5e99d32beb6371cb00df3
Marcos-Antunes-Junior <mantunes140397@gmail.com>
2024-02-20 19:09:05 -0300

    "fixed logout function"

--- src/js/addEmployee.js
+++ src/js/addEmployee.js
@@ -1,6 +1,8 @@
+import { handleLogout } from "./logout.mjs";
 import { handleAddEmployee } from "./addEmployee.mjs";
 
 const year = document.getElementById("year");
 year.innerText = `${new Date().getFullYear()}`;
 
 handleAddEmployee();
+handleLogout();
--- src/js/employee.js
+++ src/js/employee.js
@@ -1,6 +1,8 @@
+import { handleLogout } from "./logout.mjs";
 import { handleEmployeePage } from "./employee.mjs";
 
 const year = document.getElementById("year");
 year.innerText = `${new Date().getFullYear()}`;
 
 handleEmployeePage();
+handleLogout();
